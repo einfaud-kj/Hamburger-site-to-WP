12d8ec2545b67db08fde0eccd083fdd2d1674205
KYOUJI <einfaud@gmail.com>
2022-06-06 00:06:04 +0900

    サイドバー実装まであと少し・・・。

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 jQuery(function ($) {
+  $(window).resize(function () {
+    if (window.matchMedia("(min-width: 1200px)").matches) {
+      $(".js-button").removeClass("is-open");
+      $(".l-sidebar").removeClass("is-open");
+      // $(".l-sidebar").css("display", "block");
+    }
+  });
+  // $(window).resize(function () {
+  //   if (window.matchMedia("(max-width: 1200px)").matches) {
+  //     $(".l-sidebar").css("display", "none");
+  //   }
+  // });
   $(".js-button").on("click", function () {
-    $(this).toggleClass("is-open");
+    $(".js-button__sidebar").toggleClass("is-open");
     $(".l-sidebar").toggleClass("is-open");
+    // $(".l-sidebar").css("display", "block");
+  });
+  $(".js-button__sidebar").on("click", function () {
+    $(this).removeClass("is-open");
+    $(".l-sidebar").removeClass("is-open");
   });
 });
